d7403c494344230f3dfe08c0266e895542aa25f3
MaxIliv <max.r.iliv@gmail.com>
2025-10-28 14:43:30 +0200

    Simple login form

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import FormInput from './FormInput.vue'
+
+export type LoginFormData = {
+  email: string
+  password: string
+}
+
+type LoginFormEmits = {
+  submit: [payload: LoginFormData]
+}
+
+const emit = defineEmits<LoginFormEmits>()
+
+const email = ref('')
+const password = ref('')
+
+function onSubmit() {
+  emit('submit', {
+    email: email.value,
+    password: password.value,
+  })
+}
+</script>
+
+<template>
+  <form @submit.prevent="onSubmit">
+    <FormInput
+      v-model="email"
+      pattern="^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$"
+      required
+      type="email"
+      placeholder="Enter an Email"
+    />
+    <FormInput v-model="password" required type="password" placeholder="Password" />
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<style></style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import LoginForm, { type LoginFormData } from '@/components/LoginForm.vue';
+
+function onSubmit(data: LoginFormData) {
+  console.log(data);
+}
+</script>
+
+<template>
+  <div>
+    <section>
+      <h1>Please, login!</h1>
+      <LoginForm @submit="onSubmit" />
+    </section>
+  </div>
+</template>
+
+<style>
+
+</style>
